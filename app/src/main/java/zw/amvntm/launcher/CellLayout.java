99e8b4f20365bf8cf67648f66333043e645106fb
Amvntm <1515121413@qq.com>
2022-01-02 21:04:46 +0800

    Initial commit

---
+++ app/src/main/java/zw/amvntm/launcher/CellLayout.java
@@ -0,0 +1,25 @@
+package zw.amvntm.launcher;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CellLayout extends RecyclerView {
+    public CellLayout(@NonNull Context context) {
+        super(context);
+    }
+
+    public CellLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CellLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+    
+
+
+}
